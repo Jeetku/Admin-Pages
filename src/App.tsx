e0e98b8d0040eb82916d3bf2ce81ae92d02748d3
Jeetesh <jeeteshkumar45@gmail.com>
2023-11-05 19:06:03 +0530

    Folders setup and router updated with lazy loading

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Suspense, lazy } from "react";
+import Loader from "./components/Loader";
+
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Products = lazy(() => import("./pages/Products"));
+const Customer = lazy(() => import("./pages/Customer"));
+const Transaction = lazy(() => import("./pages/Transaction"));
+
+const App = () => {
+  return (
+    <>
+      <Router>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/admin/dashboard" element={<Dashboard />} />
+            <Route path="/admin/products" element={<Products />} />
+            <Route path="/admin/transaction" element={<Transaction />} />
+            <Route path="/admin/customer" element={<Customer />} />
+          </Routes>
+        </Suspense>
+      </Router>
+    </>
+  );
+};
+
+export default App;
