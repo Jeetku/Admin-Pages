d3aabc84e815c376388ff8008f3c856e5de2ec63
Jeetesh <jeeteshkumar45@gmail.com>
2023-11-06 13:16:23 +0530

    inventory

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -3,13 +3,14 @@ import AdminSidebar from "../components/AdminSidebar";
 import { BsSearch } from "react-icons/bs";
 import { HiTrendingDown, HiTrendingUp } from "react-icons/hi";
 import userImg from "../assets/userpic.png";
+import data from "../assets/data.json";
 
 const Dashboard = () => {
   return (
     <div className="adminContainer">
-      {/* SideBar */}
+      {/* ======================SideBar============================== */}
       <AdminSidebar />
-      {/* Main */}
+      {/*================== Main================================================= */}
       <main className="dashboard">
         {/* heading */}
         <div className="bar">
@@ -18,7 +19,7 @@ const Dashboard = () => {
           <FaRegBell />
           <img src={userImg} alt="User" />
         </div>
-        {/* Widget  */}
+        {/* =========================Widget===================================================  */}
         <section className="widgetContainer">
           <WidgetItem
             percent={40}
@@ -48,6 +49,26 @@ const Dashboard = () => {
             heading="Productss"
             color="rgb(76,0,255)"
           />
+        </section>
+        {/* =========================Graph ======================================================== */}
+        <section className="graphContainer">
+          <div className="revenue-chart">
+            <h2>Revenue & Transaction</h2>
+            {/* Graphs Inseted Here */}
+          </div>
+          <div className="dashboard-categories">
+            <h2>Inventory</h2>
+            <div>
+              {data.categories.map((category) => (
+                <CategoryItem
+                  key={category.heading}
+                  heading={category.heading}
+                  value={category.value}
+                  color={`hsl(${category.value * 4},100%,50%)`}
+                />
+              ))}
+            </div>
+          </div>
         </section>
       </main>
     </div>
@@ -96,6 +117,27 @@ const WidgetItem = ({
       <span style={{ color }}>{percent}%</span>
     </div>
   </article>
+);
+
+interface CategoryItemProps {
+  color: string;
+  value: number;
+  heading: string;
+}
+
+const CategoryItem = ({ color, value, heading }: CategoryItemProps) => (
+  <div className="category-item">
+    <h5>{heading}</h5>
+    <div>
+      <div
+        style={{
+          backgroundColor: color,
+          width: `${value}%`,
+        }}
+      ></div>
+    </div>
+    <span>{value}%</span>
+  </div>
 );
 
 export default Dashboard;
