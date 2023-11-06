9fc64a541e391e9b83e92526e1427f803f2da8a9
Jeetesh <jeeteshkumar45@gmail.com>
2023-11-06 11:42:47 +0530

    widget setup

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,7 @@
 import { FaRegBell } from "react-icons/fa";
 import AdminSidebar from "../components/AdminSidebar";
 import { BsSearch } from "react-icons/bs";
+import { HiTrendingDown, HiTrendingUp } from "react-icons/hi";
 import userImg from "../assets/userpic.png";
 
 const Dashboard = () => {
@@ -10,15 +11,91 @@ const Dashboard = () => {
       <AdminSidebar />
       {/* Main */}
       <main className="dashboard">
+        {/* heading */}
         <div className="bar">
           <BsSearch />
           <input type="text" placeholder="Search for data, users, docs" />
           <FaRegBell />
           <img src={userImg} alt="User" />
         </div>
+        {/* Widget  */}
+        <section className="widgetContainer">
+          <WidgetItem
+            percent={40}
+            amount={true}
+            value={340000}
+            heading="Revenue"
+            color="rgb(0,115,255)"
+          />
+          <WidgetItem
+            percent={-14}
+            amount={false}
+            value={400}
+            heading="Users"
+            color="rgb(0,198,202)"
+          />
+          <WidgetItem
+            percent={80}
+            amount={true}
+            value={23000}
+            heading="Transactions"
+            color="rgb(255,196,0)"
+          />
+          <WidgetItem
+            percent={30}
+            amount={false}
+            value={1000}
+            heading="Productss"
+            color="rgb(76,0,255)"
+          />
+        </section>
       </main>
     </div>
   );
 };
+
+interface WidgetItemProps {
+  heading: string;
+  value: number;
+  percent: number;
+  color: string;
+  amount?: boolean;
+}
+
+const WidgetItem = ({
+  heading,
+  value,
+  percent,
+  color,
+  amount,
+}: WidgetItemProps) => (
+  <article className="widget">
+    <div className="widgetInfo">
+      <p>{heading}</p>
+      <h4>{amount ? `$${value}` : value}</h4>
+      {percent > 0 ? (
+        <span className="green">
+          <HiTrendingUp />+{percent}%
+        </span>
+      ) : (
+        <span className="red">
+          <HiTrendingDown />
+          {percent}%
+        </span>
+      )}
+    </div>
+    <div
+      className="widgetCircle"
+      style={{
+        background: `conic-gradient(
+        ${color} ${(Math.abs(percent) / 100) * 360}deg,
+        rgb(255, 255, 255) 0
+      )`,
+      }}
+    >
+      <span style={{ color }}>{percent}%</span>
+    </div>
+  </article>
+);
 
 export default Dashboard;
