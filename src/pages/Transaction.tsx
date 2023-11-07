26b0c99bd14e1b85b1b2fd5958036523c18fe7af
Jeetesh <jeeteshkumar45@gmail.com>
2023-11-07 09:26:03 +0530

    Transaction setup

--- src/pages/Transaction.tsx
+++ src/pages/Transaction.tsx
@@ -1,12 +1,88 @@
+import { ReactElement, useCallback, useState } from "react";
 import AdminSidebar from "../components/AdminSidebar";
+import TableHOC from "../components/TableHoc";
+import { Link } from "react-router-dom";
+import { Column } from "react-table";
+
+interface DataType {
+  user: string;
+  amount: number;
+  discount: number;
+  quantity: number;
+  status: ReactElement;
+  action: ReactElement;
+}
+
+const columns: Column<DataType>[] = [
+  {
+    Header: "User",
+    accessor: "user",
+  },
+  {
+    Header: "Amount",
+    accessor: "amount",
+  },
+  {
+    Header: "Quantity",
+    accessor: "quantity",
+  },
+  {
+    Header: "Status",
+    accessor: "status",
+  },
+  {
+    Header: "Action",
+    accessor: "action",
+  },
+];
+
+const arr: DataType[] = [
+  {
+    user: "Charas",
+    amount: 4500,
+    discount: 400,
+    quantity: 3,
+    status: <span className="red">Processing</span>,
+    action: <Link to="/admin/transaction/sdaskdnkasjdn">Manage</Link>,
+  },
+  {
+    user: "John",
+    amount: 5500,
+    discount: 200,
+    quantity: 32,
+    status: <span className="purple ">Shipped</span>,
+    action: <Link to="/admin/transaction/sdaskdnkasjdn">Manage</Link>,
+  },
+  {
+    user: "Tom Muddy",
+    amount: 7500,
+    discount: 400,
+    quantity: 13,
+    status: <span className="green">Delivered</span>,
+    action: <Link to="/admin/transaction/sdaskdnkasjdn">Manage</Link>,
+  },
+];
 
 const Transaction = () => {
+  const [data] = useState<DataType[]>(arr);
+
+  const Table = useCallback(
+    TableHOC<DataType>(
+      columns,
+      data,
+      "dashboard-product-box",
+      "Customers",
+      true
+    ),
+    []
+  );
+
   return (
     <div className="adminContainer">
       {/* SideBar */}
       <AdminSidebar />
       {/* Main */}
-      <main>mainContent</main>
+      <main>{Table()}</main>
     </div>
   );
 };
